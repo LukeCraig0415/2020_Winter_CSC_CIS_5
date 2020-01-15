3a1374475451d95b152b9a046af1f272492d4163
rcc <rcc>
2020-01-14 17:45:24 -0800

    Signed-off-by: rcc <rcc>

--- Class/Savitch_9thEd_Chap2_Prob9_Sums/main.cpp
+++ Class/Savitch_9thEd_Chap2_Prob9_Sums/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Dr. Mark E. Lehr
  * Created on January 14, 2020, 10:30 AM
- * Purpose:  Average of 10 inputs
+ * Purpose:  Sum of 10 inputs
  *     Positive, Negative and Total Inputs
  */
 
@@ -22,111 +22,61 @@ using namespace std;
 int main(int argc, char** argv) {
     //Declare Variable Data Types and Constants
     float x;//Variable to input 10 times
-    float avgTot,avgNeg,avgPos;//Sums until the end then divide by n
-    int neg,pos,tot;//Number of negative, positive and total values input
+    float sumTot,sumNeg,sumPos;//Sums
     
     //Initialize Variables
-    x=avgTot=avgNeg=avgPos=0;
-    neg=pos=tot=0;
+    sumTot=sumNeg=sumPos=0;
     
     //Process or map Inputs to Outputs
     cout<<"Average 10 Numbers Positive or Negative"<<endl;
     cout<<"Zero is neither positive or Negative"<<endl;
     cout<<"Input a number "<<endl;
     cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
+    x>0?sumPos+=x:sumNeg+=x;
     
     cout<<"Input a number "<<endl;
     cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
+    x>0?sumPos+=x:sumNeg+=x;
     
     cout<<"Input a number "<<endl;
     cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
-    
-    cout<<"Input a number "<<endl;
-    cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
-    
-    cout<<"Input a number "<<endl;
-    cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
-    
-    cout<<"Input a number "<<endl;
-    cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
+    x>0?sumPos+=x:sumNeg+=x;
 
     cout<<"Input a number "<<endl;
     cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
+    x>0?sumPos+=x:sumNeg+=x;
 
     cout<<"Input a number "<<endl;
     cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
+    x>0?sumPos+=x:sumNeg+=x;
 
     cout<<"Input a number "<<endl;
     cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
+    x>0?sumPos+=x:sumNeg+=x;
 
     cout<<"Input a number "<<endl;
     cin>>x;
-    avgPos+=x>0?x:0;
-    pos+=x>0?1:0;
-    avgNeg+=x>0?0:x;
-    neg+=x>=0?0:1;
-    avgTot+=x;
-    tot+=1;
+    x>0?sumPos+=x:sumNeg+=x;
+
+    cout<<"Input a number "<<endl;
+    cin>>x;
+    x>0?sumPos+=x:sumNeg+=x;
+
+    cout<<"Input a number "<<endl;
+    cin>>x;
+    x>0?sumPos+=x:sumNeg+=x;
+
+    cout<<"Input a number "<<endl;
+    cin>>x;
+    x>0?sumPos+=x:sumNeg+=x;
+    
+    sumTot=sumPos+sumNeg;
 
     //Display Outputs
     cout<<fixed<<setprecision(1)<<showpoint;
-    cout<<"The average Total   = "<<avgTot/tot<<endl;
-    cout<<"The average Positive= "<<(pos>0?avgPos/pos:0)<<endl;
-    cout<<"The average Negative= "<<(neg>0?avgNeg/neg:0)<<endl;
+    cout<<"The sum Total   = "<<sumTot<<endl;
+    cout<<"The sum Positive= "<<sumPos<<endl;
+    cout<<"The sum Negative= "<<sumNeg<<endl;
     
     //Exit stage right!
     return 0;
